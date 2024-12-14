9f7b7aa4e0e6a70c6998a73f49145332a094238d
eli <jonghyung.kim@smartfoodnet.com>
2024-12-14 21:02:16 +0900

    회원가입 ui 작업

--- server/api/auth/sign-up.post.ts
+++ server/api/auth/sign-up.post.ts
@@ -2,7 +2,7 @@ import { H3Event } from 'h3';
 
 export default eventHandler(async (event: H3Event) => {
     const apiUri = process.env.FINISH_API_URL ?? 'http://localhost:9000';
-    const url = `${apiUri}/users/sign-up`;
+    const url = `${apiUri}/user/sign-up`;
     const body = await readBody(event);
 
     const response = await $fetch(url, {
