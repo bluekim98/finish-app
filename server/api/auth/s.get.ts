22b5d6579e338316fdd2bd513b898259094d426f
bluekim98 <bluekim988@gmail.com>
2025-04-17 15:18:18 +0900

    console.log 제거

--- server/api/auth/s.get.ts
+++ server/api/auth/s.get.ts
@@ -1,7 +1,4 @@
 export default eventHandler(async (event) => {
-    console.log('1@@@@@');
     const session = await useAuthSession(event);
-    console.log('alsdkfjalsdkfjalsdkfjlaskdfjalsdkfj');
-    console.log({ session: session.data });
     return session.data;
 });
